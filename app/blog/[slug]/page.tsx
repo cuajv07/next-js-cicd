d5dde326fd860031912d887e01e8d6fe4fa3d863
cuajv07 <hafit208@gmail.com>
2025-06-24 06:24:50 -0400

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -11,6 +11,5 @@ export async function generateMetadata({ params }: Params) {
 export default function Page({ params }: Params) {
   return 
   <h1>Slug: {params.slug}</h1>
-  <p>Cua</p>
-  </>;
+  ;
 }
